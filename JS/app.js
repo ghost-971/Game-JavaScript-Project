8b575916a9c7af44f5517336031db541cb433d0f
ghost-971 <57110591+ghost-971@users.noreply.github.com>
2020-10-04 13:27:02 -0400

    Apprentisage

--- JS/app.js
+++ JS/app.js
@@ -5,13 +5,15 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
 
 
 function drawBall(){
 
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
     
@@ -21,12 +23,19 @@ function draw(){
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
 
-    if(y + dy > canvas.height){
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
+        ctx.fillStyle = "blue";
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+        ctx.fillStyle ='red';
         dy = -dy;
     }
+
+    x += dx;
+    y += dy;
 
 }
 setInterval(draw, 10);
